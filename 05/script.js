a09ad4a9b92a2437f9385ac6dd2a628c89696adc
swaraj <swarajsp07@gmail.com>
2026-09-13 15:03:02 +0530

    loops done;

---
+++ 05/script.js
@@ -0,0 +1,55 @@
+console.log("loops");
+
+// for loop 
+// while loop
+// do while loop
+
+// for loop ===> when to use when we know the no of itertation
+// while loop ===> when to use when we know the condition but not know how much number of iteration it would needed 
+// do while loop ===> when to use when we want to code to execution to prints something when if the condition is false from start
+
+
+// break and contnue ======> exist , skip
+
+
+// for loop 
+
+// for (let i = 10; i<= 15 ; i++) {
+// console.log("swaraj");
+// console.log("swaraj" + i);
+// }
+// for (;;){
+//     console.log("hi");
+// } 
+
+// valid for loop but infinite 
+
+
+// when comes Infinity is when no update at all or wrong direction update added
+
+// }
+
+// while loop 
+
+// let ip = 0;
+
+// let house = 100;
+
+// while(ip != house){
+// ip = ip+1;
+// console.log("step taken:", ip);
+// }
+
+// do-while loop 
+
+let ip = 100;
+
+let house = 100;
+do {
+ip = ip+1;
+console.log("step taken:", ip);
+}
+while(ip <= house)
+
+
+
